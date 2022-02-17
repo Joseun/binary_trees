503333ec9e52a2b8cd2adce3570f343e06fb0357
Joseun <ologanj@gmail.com>
2022-02-17 14:46:24 +0000

    Binary trees with README.md with corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,28 +6,30 @@
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  *
- * Return: Pointer to a new node or NULL on failure or parent if at root
+ * Return: Pointer to a new node or NULL on failure or parent is NULL
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
-
 	binary_tree_t *newleftNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	
+
 	if (newleftNode == NULL)
 	{
 		return (NULL);
 	}
 	newleftNode->n = value;
 	newleftNode->parent = parent;
-	newleftNode->left = parent->left;
 	newleftNode->right = NULL;
+
+	if (!parent)
+	{
+		newleftNode->left = NULL;
+		return (newleftNode);
+	}
+	newleftNode->left = parent->left;
 	parent->left = newleftNode;
-	
+	newleftNode->parent = parent;
+
 	if (newleftNode->left)
 	{
 		newleftNode->left->parent = newleftNode;
