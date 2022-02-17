62589d96bb9a93cfc4a79fbca39748b96aba4911
Joseun <ologanj@gmail.com>
2022-02-17 18:10:25 +0000

    Binary trees with README.md with corrections

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,8 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newrightNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	binary_tree_t *newrightNode;
+	newrightNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
 	if (newrightNode == NULL)
 	{
