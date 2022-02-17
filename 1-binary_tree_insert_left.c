8a078305f1e16937cfd283e4fca4f7aeae720528
Joseun <ologanj@gmail.com>
2022-02-17 18:03:52 +0000

    Binary trees with README.md with corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,36 +6,26 @@
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  *
- * Return: Pointer to a new node or NULL on failure or parent is NULL
+ * Return: Pointer to a new node or NULL on failure or parent if at root
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newleftNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-
-	if (newleftNode == NULL)
-	{
-		return (NULL);
-	}
-	newleftNode->n = value;
-	newleftNode->right = NULL;
+	binary_tree_t *new;
 
 	if (!parent)
-	{
-		newleftNode->left = NULL;
-		newleftNode->parent = NULL;
-		return (newleftNode);
-	}
-	if (!parent->left)
-	{
-		newleftNode->left = NULL;
-		newleftNode->parent = parent;
-		return (newleftNode);
-	}
-	newleftNode->left = parent->left;
-	newleftNode->left->parent = newleftNode;
-	newleftNode->parent = parent;
-	parent->left = newleftNode;
-		
-	return (newleftNode);
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = parent->left;
+	parent->left = new;
+	if (new->left)
+		new->left->parent = new;
+	return (new);
 }
