adbcfd5ba74eda0706f51d133fa0c889ba81d5aa
Joseun <ologanj@gmail.com>
2022-02-17 14:35:13 +0000

    Binary trees with README.md with corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,13 +11,14 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newleftNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	
-	if (newleftNode == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	if (parent == NULL)
+
+	binary_tree_t *newleftNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	
+	if (newleftNode == NULL)
 	{
 		return (NULL);
 	}
