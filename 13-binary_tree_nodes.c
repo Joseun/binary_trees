8bfc4f10717755f70f25c41fd87e26fd6189f969
Joseun <ologanj@gmail.com>
2022-02-17 18:56:24 +0000

    Binary trees with README.md with corrections

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,16 +10,12 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
 	
-    nodes = (binary_tree_size(tree) - binary_tree_leaves(tree));
-
-    return (nodes);
+    return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
 
 /**
