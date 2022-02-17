920a65c0f88325e41572b478d8e7f34da8ba21b4
Joseun <ologanj@gmail.com>
2022-02-17 18:34:00 +0000

    Binary trees with README.md with corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,40 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height2 - function that measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to traverse
+ *
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_height2(const binary_tree_t *tree)
+{
+	size_t rDepth = 0, lDepth = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right)
+	{
+		rDepth += binary_tree_height2(tree->right);
+	}
+	if (tree->left)
+	{
+		lDepth += binary_tree_height2(tree->left);
+	}
+	if (lDepth > rDepth)
+	{
+		return (lDepth + 1);
+	}
+	else
+	{
+		return (rDepth + 1);
+	}
+}
+
+/**
  * binary_tree_height - function that measures the height of a binary tree
  *
  * @tree: pointer to the root node of the tree to traverse
@@ -16,31 +50,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 
 	return ((binary_tree_height2(tree)) - 1);
-}
-
-/**
- * binary_tree_height2 - function that measures the height of a binary tree
- *
- * @tree: pointer to the root node of the tree to traverse
- *
- * Return: 0 if tree is NULL
- */
-
-size_t binary_tree_height2(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	size_t rDepth = binary_tree_height2(tree->right);
-	size_t lDepth = binary_tree_height2(tree->left);
-
-	if (lDepth > rDepth)
-	{
-		return (lDepth + 1);
-	}
-	else
-	{
-		return (rDepth + 1);
-	}
 }
